04af398f48322e03fe2bed2bf9ab2960d72e3a6e
Jonathan Witten <jonathanwitten@willowtreeapps.com>
2018-02-07 22:18:58 -0500

    reorganize files

--- api/Controllers/drafts.js
+++
@@ -1,9 +0,0 @@
-var express = require('express')
-var router = express.Router()
-var Draft = require('../Models/draft.js')
-
-router.get('/', function (req, res) {
-  res.send({"drafts": [new Draft("a", "b", "c")]})
-})
-
-module.exports = router
--- api/Models/draft.js
+++
@@ -1,7 +0,0 @@
-function Draft(id, title, content) {
-  this.id = id
-  this.title = title
-  this.content = content
-}
-
-module.exports = Draft
